38291c664a4d226602d9236425c10592b9e89acb
Justine <justineperso03@gmail.com>
2024-03-08 09:28:54 +0100

    ajout dossiers TD2

---
+++ TD2/src/tri.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+bool is_sorted(std::vector<int> const& vec) { 
+    return std::is_sorted(vec.begin(), vec.end()); 
+}
+
+void bubble_sort(std::vector<int> & vec){
+
+}
+
---
+++ TD2/src/tri.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+bool is_sorted(std::vector<int> const& vec);
+void bubble_sort(std::vector<int> & vec);
