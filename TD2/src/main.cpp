f024a34335bc4b9a08f864a87e5ab4c03f479897
Justine <justineperso03@gmail.com>
2024-03-08 09:54:57 +0100

    ajout bubble tri

--- TD2/src/main.cpp
+++ TD2/src/main.cpp
@@ -5,11 +5,13 @@
 
 int main () {
 
-    std::vector<int> array {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::vector<int> array {6, 2, 8, 1, 5, 3, 9};
 if (is_sorted(array)) {
     std::cout << "Le tableau est trie" << std::endl;
 } else {
     std::cout << "Le tableau n'est pas trie" << std::endl;
 }
+
+bubble_sort(array);
 
 }
--- TD2/src/tri.cpp
+++ TD2/src/tri.cpp
@@ -2,11 +2,32 @@
 #include <vector>
 #include <algorithm>
 
+
+//TABLEAU TRIE OU PAS
 bool is_sorted(std::vector<int> const& vec) { 
     return std::is_sorted(vec.begin(), vec.end()); 
 }
 
-void bubble_sort(std::vector<int> & vec){
-
+//AFFICHER UN TABLEAU
+void display (std::vector<int> & vec){
+    for (int i{0}; i<vec.size();i++){
+        std::cout << vec[i] << " , " ;
+    }
 }
+
+//BUBBLE TRI
+void bubble_sort(std::vector<int> & vec){
+    while (is_sorted(vec)==false){
+        for (int i{0}; i<vec.size()-1; i++){
+            if (vec[i]>vec[i+1]){
+                int num_i = vec[i];
+                vec[i]=vec[i+1];
+                vec[i+1]=num_i;
+            }
+        }
+    }
+    display (vec);   
+}
+
+
 
